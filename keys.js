6b1c35b3723b6470b389e3232837ee343e790ec9
bgraczyk <robgrac94@gmail.com>
2021-06-29 16:58:54 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API keys - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyBVzPiDmHoW6NaKqtAC0TadJOHw_BsgGUg";
+
+// Content Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "fbb399e1493e2fd7a";
