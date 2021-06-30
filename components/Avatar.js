98e37375b751ce59900040edad4a72d8adeb7404
bgraczyk <robgrac94@gmail.com>
2021-06-30 00:10:42 -0400

    able to deploy

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,17 @@
-function Avatar({ url, className }) {
+import Image from "next/image";
+
+function Avatar({ className }) {
   return (
-    <img
-      loading="lazy"
-      className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
-      src={url}
-      alt="profile pic"
-    />
+    <>
+      <Image
+        loading="lazy"
+        className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+        src="https://firebasestorage.googleapis.com/v0/b/messenger-app-6b72d.appspot.com/o/profile.png?alt=media&token=239e5f15-e076-413b-92f7-26d4a1b1247b"
+        alt="profile pic"
+        height={40}
+        width={40}
+      />
+    </>
   );
 }
 
